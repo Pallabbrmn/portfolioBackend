d5aef1a5006bb8e57ed7092033067be142ee5485
Pallab Barman <pallabmmviii@gmail.com>
2024-05-31 00:31:59 +0530

    adding backend files

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "pallabbarman2288@gmail.com",
+    pass: "tnmd klgt xwyc qfdx",
+  },
+});
+
+app.post("/send-email", (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: email,
+    to: "pallabbarman2288@gmail.com",
+    subject: "Contact Form Submission",
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.send("Email sent: " + info.response);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
